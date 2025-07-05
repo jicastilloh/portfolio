c0caf0828934faf9612ee6e400956f938d59f6fe
Junior Castillo <castillojunior099@gmail.com>
2025-07-05 00:01:04 -0600

    Delete imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { HomeSectionComponent } from './landing/components/home-section/home-section.component';
-import { AboutMeSectionComponent } from './landing/components/about-me-section/about-me-section.component';
-import { ProjectsSectionComponent } from './landing/components/projects-section/projects-section.component';
-import { ContactSectionComponent } from './landing/components/contact-section/contact-section.component';
 
 @Component({
   selector: 'app-root',
